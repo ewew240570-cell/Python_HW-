f5515313ce8890293c87654b52eb9da0f447411a
Евгений-Р-т <ewew240570@gmail.com>
2026-03-26 22:19:24 +0300

    ыполнены задания урока 6

--- 06_lesson/lesson_6_task_1.py
+++ 06_lesson/lesson_6_task_1.py
@@ -2,25 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 driver = webdriver.Firefox()
-
 try:
-
     driver.get("http://uitestingplayground.com/ajax")
-
     button = driver.find_element(By.ID, "ajaxButton")
     button.click()
     print("Кнопка нажата, ожидание загрузки данных...")
-
     wait = WebDriverWait(driver, 20)
     success_element = wait.until(
         EC.visibility_of_element_located((By.CLASS_NAME, "bg-success"))
     )
-
     message_text = success_element.text
     print("Текст из зеленой плашки:", message_text)
-
 finally:
-
     driver.quit()
